439ae4b1c90d591a11d34275e59b402d01cbe297
Peter Bassi <peter@protect247.com>
2026-02-21 19:26:08 -0500

    check local

--- src/lib.rs
+++ src/lib.rs
@@ -12,28 +12,36 @@ impl zed::Extension for PsalmExtension {
         _language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        if let Some(path) = worktree.which("psalm-language-server") {
-            return Ok(zed::Command {
-                command: path,
-                args: vec!["--no-progress".to_string()],
-                env: worktree.shell_env(),
-            });
-        }
+        let vendor_ls = format!("{}/vendor/bin/psalm-language-server", worktree.root_path());
+        let vendor_psalm = format!("{}/vendor/bin/psalm", worktree.root_path());
 
-        if let Some(path) = worktree.which("psalm") {
-            return Ok(zed::Command {
-                command: path,
-                args: vec![
-                    "--language-server".to_string(),
-                    "--no-progress".to_string(),
-                ],
-                env: worktree.shell_env(),
-            });
-        }
+        // Check project vendor/bin first, then fall back to PATH
+        let (command, use_language_server_flag) =
+            if worktree.read_text_file("vendor/bin/psalm-language-server").is_ok() {
+                (vendor_ls, false)
+            } else if worktree.read_text_file("vendor/bin/psalm").is_ok() {
+                (vendor_psalm, true)
+            } else if let Some(path) = worktree.which("psalm-language-server") {
+                (path, false)
+            } else if let Some(path) = worktree.which("psalm") {
+                (path, true)
+            } else {
+                return Err("Could not find psalm-language-server or psalm. \
+                    Install Psalm with: composer require --dev vimeo/psalm"
+                    .to_string());
+            };
 
-        Err("Could not find psalm-language-server or psalm in PATH. \
-             Install Psalm with: composer require --dev vimeo/psalm"
-            .to_string())
+        let mut args = Vec::new();
+        if use_language_server_flag {
+            args.push("--language-server".to_string());
+        }
+        args.push("--no-progress".to_string());
+
+        Ok(zed::Command {
+            command,
+            args,
+            env: worktree.shell_env(),
+        })
     }
 }
 
