20ab01c891fd98dc4b65002f4324348972057eda
Peter Bassi <peter@protect247.com>
2026-02-21 19:32:50 -0500

    only local

--- src/lib.rs
+++ src/lib.rs
@@ -15,20 +15,17 @@ impl zed::Extension for PsalmExtension {
         let vendor_ls = format!("{}/vendor/bin/psalm-language-server", worktree.root_path());
         let vendor_psalm = format!("{}/vendor/bin/psalm", worktree.root_path());
 
-        // Check project vendor/bin first, then fall back to PATH
         let (command, use_language_server_flag) =
             if worktree.read_text_file("vendor/bin/psalm-language-server").is_ok() {
                 (vendor_ls, false)
             } else if worktree.read_text_file("vendor/bin/psalm").is_ok() {
                 (vendor_psalm, true)
-            } else if let Some(path) = worktree.which("psalm-language-server") {
-                (path, false)
-            } else if let Some(path) = worktree.which("psalm") {
-                (path, true)
             } else {
-                return Err("Could not find psalm-language-server or psalm. \
-                    Install Psalm with: composer require --dev vimeo/psalm"
-                    .to_string());
+                return Err(format!(
+                    "Could not find psalm-language-server or psalm in {}/vendor/bin/. \
+                     Install Psalm with: composer require --dev vimeo/psalm",
+                    worktree.root_path()
+                ));
             };
 
         let mut args = Vec::new();
@@ -36,6 +33,8 @@ impl zed::Extension for PsalmExtension {
             args.push("--language-server".to_string());
         }
         args.push("--no-progress".to_string());
+
+        println!("psalm-zed: running {} {}", command, args.join(" "));
 
         Ok(zed::Command {
             command,
